ad04d4ecfb68070932188074f940c2d0b48d0f35
Tim Koopmans <tim.koops@gmail.com>
2023-08-22 20:25:35 +1000

    try cookies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import './App.css';
 import { useAuthUser } from "@frontegg/react";
+import React, { useState, useEffect } from 'react';
 
 function App() {
+    const [cookieValue, setCookieValue] = useState(null);
+
+    useEffect(() => {
+        // On component mount, read the cookie value
+        const value = getCookie("loginOrigin");
+        setCookieValue(value);
+    }, []);
+
+    const getCookie = (name) => {
+        const nameEQ = name + "=";
+        const ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    };
+
     const user = useAuthUser();
     console.log('user', user);
     const logout = () => {
@@ -12,6 +32,12 @@ function App() {
     if (redirectUrl) {
         console.log('got redirectUrl', redirectUrl);
         localStorage.setItem('redirectUrl', redirectUrl);
+    }
+
+    let loginOrigin = cookieValue;
+    if (loginOrigin) {
+        console.log('got loginOrigin', loginOrigin);
+        localStorage.setItem('redirectUrl', loginOrigin);
     }
 
     console.log('user', user);
@@ -35,6 +61,7 @@ function App() {
             <div className="logout-section">
                 <button className="logout-button" onClick={() => logout()}>Logout</button>
             </div>
+            <p>Value of 'loginOrigin' cookie: {cookieValue}</p>
         </div>
     );
 }
